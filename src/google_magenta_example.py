22c33799c1747f04c87f7a7fb421627a68b2918b
Ethan Jones <61836666+ejones18@users.noreply.github.com>
2021-09-15 10:31:17 +0100

    Update google_magenta_example.py

--- src/google_magenta_example.py
+++ src/google_magenta_example.py
@@ -7,7 +7,7 @@ import cv2
 
 def parse_options():
     """Parse command line options."""
-    parser = argparse.ArgumentParser(description=("This is the CLI for the run_gan module"),
+    parser = argparse.ArgumentParser(description=("This is the CLI for the google_magenta_example module"),
                                      epilog="Ethan Jones, 2021-08-31")
     parser.add_argument("-c", "--content-image", dest="content_img", action="store", type=str,
                         required=True)
@@ -17,25 +17,20 @@ def parse_options():
     return options
     
 def main(content_img_path, style_img_path):
-    # Load content and style images (see example in the attached colab).
+    # Load content and style images
     content_image = plt.imread(content_img_path)
     style_image = plt.imread(style_img_path)
-    # Convert to float32 numpy array, add batch dimension, and normalize to range [0, 1]. Example using numpy:
+    # Convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
     content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
     style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
-    # Optionally resize the images. It is recommended that the style image is about
-    # 256 pixels (this size was used when training the style transfer network).
-    # The content image can be any size.
-    #style_image = tf.image.resize(style_image, (256, 256))
-
-    # Load image stylization module.
+    # Load image stylisation module.
     hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-
-    # Stylize image.
+    # Stylise image.
     outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
     stylised_image = outputs[0]
+    # Save image.
     cv2.imwrite("./images/generated_image.jpg", cv2.cvtColor(np.squeeze(stylised_image)*255,
-                                                    cv2.COLOR_BGR2RGB))
+                                                             cv2.COLOR_BGR2RGB))
 
 if __name__ == "__main__":
     options = parse_options()
